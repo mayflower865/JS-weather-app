b77c181f162b57e7396f8813ad3abcdc541a4a11
mayflower865 <mayflowerlongfellow@gmail.com>
2022-12-31 10:15:39 -0600

    changing api key

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "25fad9f7e87157d33dde0f82ab269ee8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
