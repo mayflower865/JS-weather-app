b292225f4a4e7b72e61d040acbe3fc74f863ac38
mayflower865 <mayflowerlongfellow@gmail.com>
2022-12-31 02:26:22 -0600

    capitalizing the days of the week

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 
   return days[day];
 }
