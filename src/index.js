7f8c85b54a795424919c4915e240cefd8c0d9fae
mayflower865 <mayflowerlongfellow@gmail.com>
2023-01-02 13:04:51 -0600

    updated API Key

--- src/index.js
+++ src/index.js
@@ -100,7 +100,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
